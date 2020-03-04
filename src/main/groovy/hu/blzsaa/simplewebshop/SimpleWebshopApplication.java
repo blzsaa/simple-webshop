1b5b1ad2129714b6affc3e96f255b7293a713ace
Balazs Veres <balazs.veres@yahoo.com>
2020-03-04 16:26:02 +0100

    init

---
+++ src/main/groovy/hu/blzsaa/simplewebshop/SimpleWebshopApplication.java
@@ -0,0 +1,12 @@
+package hu.blzsaa.simplewebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebshopApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleWebshopApplication.class, args);
+  }
+}
---
+++ src/test/groovy/hu/blzsaa/simplewebshop/it/SimpleWebshopApplicationIT.java
@@ -0,0 +1,11 @@
+package hu.blzsaa.simplewebshop.it;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebshopApplicationIT {
+
+  @Test
+  void contextLoads() {}
+}
